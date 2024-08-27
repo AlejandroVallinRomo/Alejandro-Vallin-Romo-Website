0ca511f29722ecece64a14e8a8913db37c6f7c99
AlexChido <alva2121@colorado.edu>
2024-08-26 21:14:46 -0600

    added pictures on two projects, changed skills section, ready to add images on the churn rate project

--- scripts.js
+++ scripts.js
@@ -7,4 +7,33 @@ document.querySelectorAll('.project-title').forEach(title => {
             const content = title.nextElementSibling;
             content.style.display = content.style.display === 'block' ? 'none' : 'block';
         });
-    });														
+    });
+
+// Image Lightbox:
+
+document.querySelectorAll('.image-gallery img').forEach(function(image) {
+    image.addEventListener('click', function() {
+        // Get the source of the clicked image
+        var imgSrc = this.src;
+        var imgCaption =this.getAttribute('data-caption');
+        
+        // Update the lightbox image source
+        var lightboxImg = document.getElementById('lightbox-img');
+        lightboxImg.src = imgSrc;
+        
+        //Update lightbox caption
+        var lightboxCaption = document.querySelector('.lightbox-caption');
+        lightboxCaption.textContent= imgCaption;
+        
+
+        // Show the lightbox
+        var lightbox = document.getElementById('lightbox');
+        lightbox.style.display = 'block';
+    });
+});
+
+// Close the lightbox
+document.querySelector('.lightbox .close').addEventListener('click', function() {
+    var lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+});	
